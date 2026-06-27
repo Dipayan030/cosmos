f256b61710563b6c3b22d1911bb96cbcc9d64341
Dipayan030 <dipayan030@gmail.com>
2026-06-28 00:58:31 +0530

    url path updated 5.0

--- src/hooks/solarSystem.js
+++ src/hooks/solarSystem.js
@@ -6,6 +6,7 @@ function solarSystem(planetName) {
         const fetchSolarSystem = async (planetName) => {
             try {
                 const API_KEY = import.meta.env.VITE_SOLAR_API_KEY;
+                const isLocalDev = import.meta.env.DEV;
                 const targetUrl = `https://api.le-systeme-solaire.net/rest/bodies/${planetName}`;
                 const fetchUrl = isLocalDev 
                     ? `/api/rest/bodies/${planetName}` 
