a51857ba2f7a6c73d3d6fbe97edbba921d7d65d9
Dipayan030 <dipayan030@gmail.com>
2026-07-09 00:27:02 +0530

    Onload numbers count up animation added from react-bits

--- frontend/src/components/Planet.jsx
+++ frontend/src/components/Planet.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom"
 import destinationData from "../data/mockData"
 import Sidebar from "./Sidebar"
+import CountUp from "./CountUp"
 
 export default function Planet(){
     const {id} = useParams();
@@ -18,19 +19,63 @@ export default function Planet(){
                 <Link className="self-end lg:self-center" to={`/book/${id}`}><button className="text-sm lg:text-md h-12 w-32 lg:h-16 lg:w-42 border border-white/70 text-white/90 font-space-grotesk hover:bg-white hover:text-black transition-colors transition-discrete ease-in-out duration-200">Book a Trip</button></Link>
                 <div className="grid grid-cols-2 lg:grid-cols-4 text-white justify-around gap-8 lg:gap-16">
                     <span className="text-center flex flex-col gap-2 lg:gap-3">
-                        <h1 className="text-xl lg:text-4xl font-space-mono">{destinationData[id].equaRadius}<span className="text-sm lg:text-lg font-space-grotesk"> KM</span></h1>
+                        <h1 className="text-xl lg:text-4xl font-space-mono">
+                            <CountUp
+                                from={0}
+                                to={destinationData[id].equaRadius}
+                                separator=","
+                                direction="up"
+                                duration={0.05}
+                                className="count-up-text"
+                                delay={0}
+                            />
+                            <span className="text-sm lg:text-lg font-space-grotesk"> KM</span>
+                        </h1>
                         <p className="text-[#6E6E6E] text-sm lg:text-lg">EQUATORIAL RADIUS</p>
                     </span>
                     <span className="text-center flex flex-col gap-2 lg:gap-3">
-                        <h1 className="text-xl lg:text-4xl font-space-mono">{destinationData[id].oritalPeriod}<span className="text-sm lg:text-lg font-space-grotesk"> EARTH DAYS</span></h1>
+                        <h1 className="text-xl lg:text-4xl font-space-mono">
+                            <CountUp
+                                from={0}
+                                to={destinationData[id].oritalPeriod}
+                                separator=","
+                                direction="up"
+                                duration={0.05}
+                                className="count-up-text"
+                                delay={0}
+                            />
+                            <span className="text-sm lg:text-lg font-space-grotesk"> EARTH DAYS</span>
+                        </h1>
                         <p className="text-[#6E6E6E] text-sm lg:text-lg">ORBITAL PERIOD</p>
                     </span>
                     <span className="text-center flex flex-col gap-2 lg:gap-3">
-                        <h1 className="text-xl lg:text-4xl font-space-mono">{destinationData[id].density}<span className="text-sm lg:text-lg font-space-grotesk"> G/CM³</span></h1>
+                        <h1 className="text-xl lg:text-4xl font-space-mono">
+                            <CountUp
+                                from={0}
+                                to={destinationData[id].density}
+                                separator=","
+                                direction="up"
+                                duration={0.05}
+                                className="count-up-text"
+                                delay={0}
+                            />
+                            <span className="text-sm lg:text-lg font-space-grotesk"> G/CM³</span>
+                        </h1>
                         <p className="text-[#6E6E6E] text-sm lg:text-lg">MASS DENSITY</p>
                     </span>
                     <span className="text-center flex flex-col gap-2 lg:gap-3">
-                        <h1 className="text-xl lg:text-4xl font-space-mono">{destinationData[id].solarAphelion}<span className="text-sm lg:text-lg font-space-grotesk"> MIL KM</span></h1>
+                        <h1 className="text-xl lg:text-4xl font-space-mono">
+                            <CountUp
+                                from={0}
+                                to={destinationData[id].solarAphelion}
+                                separator=","
+                                direction="up"
+                                duration={0.05}
+                                className="count-up-text"
+                                delay={0}
+                            />
+                            <span className="text-sm lg:text-lg font-space-grotesk"> MIL KM</span>
+                        </h1>  
                         <p className="text-[#6E6E6E] text-sm lg:text-lg">SOLAR APHELION</p>
                     </span>
 
