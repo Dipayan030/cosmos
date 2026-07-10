790265efb2173cd440ac98d8b369d03b4b89283c
Dipayan030 <dipayan030@gmail.com>
2026-07-10 13:29:34 +0530

    onload animation added to destination page

--- frontend/src/components/Planet.jsx
+++ frontend/src/components/Planet.jsx
@@ -3,20 +3,27 @@ import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom"
 import destinationData from "../data/mockData"
 import Sidebar from "./Sidebar"
-import CountUp from "./CountUp"
+import CountUp from "./react-bits/CountUp"
+import FadeContent from "./react-bits/FadeContent"
 
 export default function Planet(){
     const {id} = useParams();
     return(
         <div style={{ backgroundImage: `url(${destinationData[id].img})` }} className="bg-black h-screen max-w-screen p-6 sm:p-12 lg:p-28 bg-cover bg-center bg-no-repeat flex flex-col justify-end items-end gap-8 lg:gap-18 transition-all duration-500 ease-in-out overflow-hidden">
             <Sidebar/>
-            <span className="font-space-grotesk flex flex-col gap-5 transition-all duration-700 ease-in-out">
-                <h1 className="text-4xl lg:text-7xl text-white text-right font-medium">{destinationData[id].name}</h1>
-                <h2 className="text-right text-[#6E6E6E] text-sm lg:text-xl">ORBITAL PARAMETERS RETRIEVED // LIVE SECURE LINK</h2>
-                <p className="text-sm lg:text-lg text-right lg:w-3/6 self-end text-white/70">{destinationData[id].desc}</p>
-            </span>
+            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                <span className="font-space-grotesk flex flex-col gap-5 transition-all duration-700 ease-in-out">
+                    <h1 className="text-4xl lg:text-7xl text-white text-right font-medium">{destinationData[id].name}</h1>
+                    <h2 className="text-right text-[#6E6E6E] text-sm lg:text-xl">ORBITAL PARAMETERS RETRIEVED // LIVE SECURE LINK</h2>
+                    <p className="text-sm lg:text-lg text-right lg:w-3/6 self-end text-white/70">{destinationData[id].desc}</p>
+                </span>
+            </FadeContent>
             <span className="flex flex-col gap-12 lg:gap-0 lg:flex lg:flex-row lg:justify-between w-full lg:items-center">
-                <Link className="self-end lg:self-center" to={`/book/${id}`}><button className="text-sm lg:text-md h-12 w-32 lg:h-16 lg:w-42 border border-white/70 text-white/90 font-space-grotesk hover:bg-white hover:text-black transition-colors transition-discrete ease-in-out duration-200">Book a Trip</button></Link>
+                <Link className="self-end lg:self-center" to={`/book/${id}`}>
+                     <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                        <button className="text-sm lg:text-md h-12 w-32 lg:h-16 lg:w-42 border border-white/70 text-white/90 font-space-grotesk hover:bg-white hover:text-black transition-colors transition-discrete ease-in-out duration-200">Book a Trip</button>
+                    </FadeContent>
+                </Link>  
                 <div className="grid grid-cols-2 lg:grid-cols-4 text-white justify-around gap-8 lg:gap-16">
                     <span className="text-center flex flex-col gap-2 lg:gap-3">
                         <h1 className="text-xl lg:text-4xl font-space-mono">
